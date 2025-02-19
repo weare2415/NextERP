47aee8d539569c82f0d69ab18c4cec3b28cde888
paesir <jay6423@gmail.com>
2025-02-19 18:07:57 +0900

    메인페이지 구성 1차 수정

--- frontend/src/common/pages/MainPage.js
+++ frontend/src/common/pages/MainPage.js
@@ -7,6 +7,7 @@ import MonthlySalesChart from '../../test/MonthlySalesChart';
 import MonthlyProductSalesPieChart
 	from '../../test/MonthlyProductSalesPieChart';
 import QuarterProfitRateChart from '../../test/QuaterProfitRateChart';
+import MonthlyCashFlowChart from '../../test/MonthlyCashFlowChart';
 
 const MainPage = () => {
 	const {isLogin, moveToLogin} = useCustomLogin()
@@ -25,33 +26,40 @@ const MainPage = () => {
 								<div>
 									<div style={{
 										display: "flex",
-										justifyContent: "space-between",
+										justifyContent: "center",
 										alignItems: "flex-start",
-										marginLeft: '1rem'
+										marginLeft: '1rem',
+										height:'450px'
 									}}>
-										<div>
-											<div style={{fontSize: '2rem'}}>공지사항</div>
-											<div style={{width: '90%', fontSize: '0.75rem'}}>
-												<AnnouncementList/>
+										<div style={{width:'100%', marginRight: '1rem', marginTop:'-2rem'}}>
+											{/*<div style={{fontSize: '2rem'}}>영업이익</div>*/}
+											<div>
+											<MonthlyCashFlowChart />
 											</div>
 										</div>
+									</div>
+									<div>
+										<div>
+											<div>
+												<div style={{fontSize: '2rem', marginBottom:'0.2rem'}}>제품 월별 매출 현황</div>
+												<MonthlyProductSalesPieChart/></div>
+										</div>
+									</div>
+									<div style={{display: 'flex', justifyContent: 'center', marginTop:'1rem'}}>
+										<div style={{width:'100%'}}>
+											<div style={{fontSize: '2rem', marginBottom:'1rem' }}>월별 매출 현황</div>
+											<MonthlySalesChart/></div>
+
+{/*										<div style={{width:'100%'}}>
+											<div style={{fontSize: '2rem', marginBottom:'1rem'}}>분기별 매출 및 영업이익</div>
+											<QuarterProfitRateChart/></div>*/}
+
 										<div>
 										<div style={{fontSize: '2rem'}}>캘린더</div>
 										<div style={{marginTop: '1.3rem', marginRight: '1rem'}}>
 											<CalendarComponent/>
 										</div>
 										</div>
-									</div>
-									<div style={{display: 'flex', justifyContent: 'center', marginTop:'3rem'}}>
-										<div style={{width:'100%'}}>
-											<div style={{fontSize: '2rem', marginBottom:'1rem' }}>월별 매출 현황</div>
-											<MonthlySalesChart/></div>
-										<div style={{width:'20%'}}>
-											<div style={{fontSize: '2rem', marginBottom:'1rem'}}>분기별 매출 및 영업이익</div>
-											<QuarterProfitRateChart/></div>
-										<div>
-											<div style={{fontSize: '2rem', marginBottom:'1rem'}}>제품 월별 매출 현황</div>
-											<MonthlyProductSalesPieChart/></div>
 									</div>
 								</div>
 							</div>
